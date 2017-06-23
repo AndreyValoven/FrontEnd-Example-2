8a02cde64fe46c19fc8b6e5287bc8fd3f35b89b7
Andy <scorpion.and@ukr.net>
2017-06-23 17:46:44 +0300

    js for navbar for mobile it`s not work now

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
-/**
- * Created by andy on 09.06.17.
- */
+
+//
+// document.getElementsByClassName('mob-icon').onclick = navBar;
+//
+//
+// function navBar () {
+//     document.getElementsByClassName("navbar").style.display = "block";
+// }
+
+// (function () {
+//    var navBar = $(".navbar")
+//        $(".mob-icon").on('click',function (e) {
+//        navBar.toggleClass("navbar-mobile");
+//        e.preventDefault();
+//    })
+// });
+
+
+
+document.getElementsByClassName("mob-icon").onclick = function (e) {
+    document.getElementById("navBar").className += " navbar-mob"
+
+}
