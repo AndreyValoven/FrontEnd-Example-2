bd3ad4e50a1c2d5f3698df6a09210df785e27175
Andy <scorpion.and@ukr.net>
2017-06-23 17:47:29 +0300

    gulp and npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify  = require('gulp-uglifyjs');
 
 gulp.task('sass', function() {
-    return gulp.src('app/sass/**/*.+(scss|sass)')
+    return gulp.src('app/sass/main.sass') 
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('sync', function() {
+
+gulp.task('scripts', function () {
+   return gulp.src('app/js/*.js')
+       .pipe(concat(('main.min.js')))
+       .pipe(uglify())
+       .pipe(gulp.dest("app/js"));
+});
+
+
+gulp.task('browser-sync', function() {
     browserSync({
         server: {
             baseDir: 'app'
@@ -20,7 +31,9 @@ gulp.task('sync', function() {
 
 
 
-gulp.task('watch', ['sass', 'sync'], function() {
+
+
+gulp.task('watch', ['sass', 'browser-sync'], function() {
     gulp.watch('app/sass/**/*.+(scss|sass)',['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
