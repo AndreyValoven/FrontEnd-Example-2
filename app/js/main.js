27e8f28300561c0056d89118b39045826550e30b
AndyScorp <scorpion.and@ukr.net>
2017-06-30 10:14:48 +0300

    Fix mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,11 @@
-
-//
-// document.getElementsByClassName('mob-icon').onclick = navBar;
-//
-//
-// function navBar () {
-//     document.getElementsByClassName("navbar").style.display = "block";
-// }
-
-// (function () {
-//    var navBar = $(".navbar")
-//        $(".mob-icon").on('click',function (e) {
-//        navBar.toggleClass("navbar-mobile");
-//        e.preventDefault();
-//    })
-// });
-
-
-
-document.getElementsByClassName("mob-icon").onclick = function (e) {
-    document.getElementById("navBar").className += " navbar-mob"
-
-}
+$(function() {
+    
+    $('.mob-icon').on('click', function() {
+        
+        $('.navbar').slideToggle(300, function() {
+            if($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+    })
+})
